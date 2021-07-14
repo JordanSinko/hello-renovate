f9a417b332aa8b8eae9ee1f3815153d14ea19c0b
Jordan Sinko <jordan5sinko@gmail.com>
2021-07-14 10:02:19 -0400

    chore: adding handler dependencies

--- packages/my-handler/index.js
+++ packages/my-handler/index.js
@@ -1,1 +1,3 @@
-exports.handler = async () => `Hello from Lambda`;
+const sample = require("lodash.sample");
+const greetings = ["Hello", "Hi", "Greetings", "Howdy", "Bonjour"];
+exports.handler = async () => `${sample(greetings)} from Lambda`;
